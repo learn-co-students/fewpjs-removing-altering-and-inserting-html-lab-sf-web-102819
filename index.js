2bf5a6fd34554c6bc2b258745828d0a342a91eae
Your_Name <joseph161216@gmail.com>
2019-12-09 08:14:31 -0800

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+const mainEle = document.querySelector("#main")
+mainEle.remove()
+
+const newHeader = document.createElement("h1")
+newHeader.id = "victory"
+newHeader.innerHTML = "Joseph is the champion"
