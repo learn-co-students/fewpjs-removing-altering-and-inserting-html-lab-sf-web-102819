50d582d1816ae92bbc85a7ded1201e93b50d3bed
Allen Shin <geeuho@gmail.com>
2019-12-09 17:36:40 -0800

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Write your code here!
+// Write your code here! 
+let main = document.querySelector('main#main')
+main.remove();
+
+let newHeader = document.querySelector('h1#victory')
+newHeader.innerHTML = `<h1>Jesus is the champion</h1>`
