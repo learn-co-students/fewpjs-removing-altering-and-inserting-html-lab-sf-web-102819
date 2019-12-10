f57997b8177aec9f03ae85c08e6e0b2044cc4485
Will Diep <widiep@gmail.com>
2019-12-10 14:06:39 -0800

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your code here!
+const main = document.querySelector('main')
+
+main.remove()
+
+const newHeader = document.createElement('h1')
+
+newHeader.id = "victory"
+
+document.body.append(newHeader.innerHTML = "YOUR-NAME is the champion")
+document.write(newHeader.innerHTML = "YOUR-NAME is the champion")
+// const newHeader = document.querySelector('h1#victory')
